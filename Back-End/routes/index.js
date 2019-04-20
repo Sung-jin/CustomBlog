4e477038918deb32def563709b60bfe91f3fe5fe
SungJin <osj4872@gmail.com>
2019-04-20 18:30:53 +0900

    #4 Back-End 프로젝트 환경 기본 세팅 See: Github issue #4 --------------------------------------- ### 작업파일 * routes 아래 파일 * test 파일 test 폴더 아래로 이동 * package.json --------------------------------------- ### 작업내용 * npm script 각각 용도에 맞게 다시 변경 * 폴더 구분 * Git History 꼬인거 해결 * TDD 초기화 작업 완료

--- Back-End/routes/index.js
+++ Back-End/routes/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello World 3333');
+  res.send('Hello World');
 });
 
 module.exports = router;
