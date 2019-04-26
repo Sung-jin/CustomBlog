2f3a512a2c5f045732b2f08c61b5a2c925bce138
SungJin <osj4872@gmail.com>
2019-04-26 17:41:13 +0900

    deploy 테스트 2

--- Back-End/deploy.sh
+++ Back-End/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git config --gloobal push.default simple
+git config --global push.default simple
 git remote add production ssh://fonnie@120.50.78.185/CustomBlog
 git push production deploy
