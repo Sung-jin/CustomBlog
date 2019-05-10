2060965363f48291e2f5002da1346471768775ef
SungJin <osj4872@gmail.com>
2019-05-10 20:40:18 +0900

    travis deploy test

--- Back-End/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#kill $(lsof -t -i:3200)
-mkdir ~/deploy && cd ~/deploy
-git clone -b deploy --single-branch https://github.com/Sung-jin/CustomBlog.git
-cd CustomBlog/Back-End && cp -rf routes ~/CustomBlog/Back-End
-cd ~ && rm -rf deploy
